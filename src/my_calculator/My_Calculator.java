1d5564f464f935db01658276d74396185d82df59
tarek-10 <tarek.mohamed105900@gmail.com>
2021-11-04 04:26:05 +0200

    creating calcalutor using java

---
+++ src/my_calculator/My_Calculator.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my_calculator;
+
+/**
+ *
+ * @author hp
+ */
+public class My_Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmCalC F = new FrmCalC();
+        
+        F.setVisible(true);
+        F.setLocationRelativeTo(null);
+    }
+    
+}
